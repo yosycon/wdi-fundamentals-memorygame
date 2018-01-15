4769d39edca760fe4a43478fa4b39f946f03f01e
yosycon <34780107+yosycon@users.noreply.github.com>
2018-01-15 13:28:27 -0800

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-//create variable for cards user flipped
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-
-var cardTwo = cards[1];
-cardsInPlay.push('cardTwo');
-
-//check length of cardsinplay is 2
-if (cardsInPlay[0] === cardsInPlay[1]) {
-  alert("you found a match!");
-} else {
-  alert("sorry, try again");
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+    alert("you found a match!");
+    } else {
+    alert("sorry, try again");
+    }
 }
 
-console.log ("user flipped " + cardOne);
-console.log ("user flipped " + cardTwo);
+var flipCard = function(cardId) {
+  cardsInPlay.push(cards[cardId]);
+  console.log ("user flipped " + cards[cardId]);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(1);
