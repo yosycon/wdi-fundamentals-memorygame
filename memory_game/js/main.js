f6d816282db29654cbfb0833fed028526975ae06
yosycon <34780107+yosycon@users.noreply.github.com>
2018-01-16 00:36:36 -0800

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,30 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-  if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("you found a match!");
     } else {
     alert("sorry, try again");
@@ -10,10 +32,12 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-  cardsInPlay.push(cards[cardId]);
-  console.log ("user flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log ("user flipped " + cards[cardId].rank);
+  console.log (cards[cardId].cardImage);
+  console.log (cards[cardId].suit);
   checkForMatch();
 }
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
