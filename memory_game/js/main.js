e27e6b60781191c6853b400332a3e4836e68d969
yosycon <34780107+yosycon@users.noreply.github.com>
2018-01-22 22:37:34 -0800

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,21 +24,34 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-  if (cardsInPlay.length === 2)
+  if (cardsInPlay.length === 2) {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("you found a match!");
     } else {
     alert("sorry, try again");
+
     }
+  }
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id');
+this.setAttribute('src', cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
   console.log ("user flipped " + cards[cardId].rank);
   console.log (cards[cardId].cardImage);
   console.log (cards[cardId].suit);
-  checkForMatch();
+checkForMatch();
 }
 
-flipCard(0);
-flipCard(1);
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+createBoard();
